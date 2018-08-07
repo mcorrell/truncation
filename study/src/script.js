1221f3d4ef5efc8f36b841eadabf3150288f0fc4
mcorrell <michael.a.correll@gmail.com>
2018-08-06 18:05:31 -0700

    Initial generation code

--- study/src/script.js
+++ study/src/script.js
@@ -11,46 +11,93 @@ Study Parameters
 var allVisTypes = ["bar","gradbar","bottomgradbar","scatter","lollipop","gradlollipop","gradbottomlollipop","pointline","line","area","gradarea","bottomgradarea"];
 
 //What type of visualizations will they see?
-var visTypes = ["bar","bottomgradbar","scatter","lollipop","gradbottomlollipop","pointline","area","bottomgradarea"];
+var visTypes = ["bar","pointline","area"];
 
 //Will the vis be labeled?
-var labelTypes = ["none","top","with"];
-
-//Will the y-axis be futzed with? If so, where will it start?
-var truncationTypes = [0,0.2,0.4,0.8];
+var labelTypes = ["none"];
 
 //How will the task questions be framed? In terms of specific values, in terms of the overall trend, or a mix of both?
-var framingTypes = ["values","trend","mix"];
+var framingTypes = ["values","trend"];
 
-var makeStimuli = function(permute){
-  //Populate the trial stimuli, optionally permuting the trials
-  var stimuli = [];
-  var replicates = 1;
-  var trainingReplicates = 1;
-  var stimulis;
-  var id=gup("id");
-  id = id ? id : "EMPTY";
-  var index = 1;
-  var i;
-  //currently all blocked effects. We'd potentially want some of these to be random.
+//How many items are in the series?
+var dataSizes = [2,3];
 
-  d3.select("#progress").html("Question 1/"+stimuli.length);
-  return stimuli;
-}
+//Will the y-axis be futzed with? If so, where will it start?
+var truncationTypes = [0,0.5];
 
+//Are the data going up or down?
+var trendDirections = [1];
+
+//How much are they going down or up by?
+var trendSlopes = [0.25,0.5,1];
+
+
+/***
+Global Variables and Settings
+***/
 var participantData = [];
 var rt;
 var stimuli = makeStimuli(true);
 var questionIndex = 0;
 
+
+//The actual visual properties of our visualizations
 var vizWidth = 400;
 var vizHeight = 300;
 var margin = 40;
 var markSize = 10;
-
+var lineWidth = 4;
 var x = d3.scaleBand().domain([0,1]).range([margin,vizWidth]).paddingOuter(0.2).paddingInner(0.1);
 var y = d3.scaleLinear().domain([0,1]).range([vizHeight-margin,margin]);
 var yAxis = d3.axisLeft(y).tickFormat(d3.format(".0%")).tickValues([0,1]);
+
+
+function makeStimuli(permute){
+  //Populate the trial stimuli, optionally permuting the trials
+  var stimuli = [];
+  var replicates = 1;
+  var stimulis;
+  var id=gup("id");
+  id = id ? id : "EMPTY";
+  var index = 1;
+  //currently all blocked effects. We'd potentially want some of these to be random.
+  for(vis of visTypes){
+    for(label of labelTypes){
+      for(frame of framingTypes){
+        for(size of dataSizes){
+          for(truncation of truncationTypes){
+            for(direction of trendDirections){
+              for(slope of trendSlopes){
+                stimulis = {};
+                stimulis.visType = vis;
+                stimulis.labelType = label;
+                stimulis.framing = frame;
+                stimulis.dataSize = size;
+                stimulis.truncationLevel = truncation;
+                stimulis.trendDirection = direction;
+                stimulis.trendSlope = slope;
+                stimulis.data = genData(stimulis);
+                stimuli.push(stimulis);
+                index++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  if(permute){
+    dl.permute(stimuli);
+  }
+
+  stimuli.forEach(function(d,i){
+    d.i = i;
+  });
+
+  d3.select("#progress").html("Question 1/"+stimuli.length);
+  return stimuli;
+}
 
 
 /***
@@ -96,6 +143,25 @@ var nextQuestion = function(){
 /***
 Viz Functions
 ***/
+var makeVis = function(stimulis){
+  var draw = getVisFunction(stimulis.visType);
+  truncate(stimulis.truncationLevel);
+  draw(d3.select("#vis"),stimulis.data);
+  switch(stimulis.labelType){
+    case "with":
+    drawLabels(d3.select("#vis"));
+    break;
+
+    case "above":
+    drawTopLabels(d3.select("#vis"));
+    break;
+
+    case "none":
+    default:
+    break;
+  }
+}
+
 
 var scatter = function(svg,data){
   //scatterplot
@@ -193,7 +259,7 @@ var lollipop = function(svg,data){
     .attr("x2", (d,i) => x(i) + (x.bandwidth()/2))
     .attr("y1", y(y.domain()[0]))
     .attr("y2", d => y(d))
-    .attr("stroke-width","4px");
+    .attr("stroke-width",lineWidth);
 
   svg.append("g")
     .attr("transform","translate(" + margin + ",0)")
@@ -215,9 +281,9 @@ var gradLollipop = function(svg,data){
 
   g.selectAll("rect").data(data).enter().append("rect")
     .attr("fill","url(#grad)")
-    .attr("x", (d,i) => x(i)-2 + (x.bandwidth()/2))
+    .attr("x", (d,i) => x(i)-(lineWidth/2) + (x.bandwidth()/2))
     .attr("y",d => y(d))
-    .attr("width","4px")
+    .attr("width",lineWidth)
     .attr("height",d => y(y.domain()[0]) - y(d));
 
   svg.append("g")
@@ -244,13 +310,13 @@ var gradBottomLollipop = function(svg,data){
     .attr("x2", (d,i) => x(i) + (x.bandwidth()/2))
     .attr("y1", y(y.domain()[0]))
     .attr("y2", d => y(d))
-    .attr("stroke-width","4px");
+    .attr("stroke-width",lineWidth);
 
   g.selectAll("rect").data(data).enter().append("rect")
     .attr("fill","url(#grad)")
-    .attr("x", (d,i) => x(i)-2 + (x.bandwidth()/2))
+    .attr("x", (d,i) => x(i)-(lineWidth/2) + (x.bandwidth()/2))
     .attr("y",vizHeight-margin)
-    .attr("width","4px")
+    .attr("width",lineWidth)
     .attr("height",margin);
 
   svg.append("g")
@@ -333,7 +399,7 @@ var line = function(svg,data){
 
   svg.append("path").datum(data)
     .attr("stroke","#333")
-    .attr("stroke-width","4px")
+    .attr("stroke-width",lineWidth)
     .attr("fill","none")
     .attr("d",path);
 
@@ -355,7 +421,7 @@ var pointLine = function(svg,data){
 
   g.append("path").datum(data)
     .attr("stroke","#333")
-    .attr("stroke-width","4px")
+    .attr("stroke-width",lineWidth)
     .attr("fill","none")
     .attr("d",path);
 
@@ -457,7 +523,7 @@ function truncate(val){
 Test and Debug Functions
 ***/
 
-function changeVisType(newViz,newParameter){
+var changeVisType = function(newViz,newParameter){
   //change the current vis that is in "#vis" to a different one, preserving whatever data was there.
   var makeViz = getVisFunction(newViz);
   var data = d3.select("svg").datum();
@@ -465,14 +531,14 @@ function changeVisType(newViz,newParameter){
   makeViz(d3.select("svg"),data,newParameter);
 }
 
-function testVis(vizType){
+var testVis = function(vizType){
   //make a vis of the current type in #vis, with random data.
   var makeViz = getVisFunction(vizType);
   d3.select("#vis").selectAll("*:not(.grad)").remove();
   makeViz(d3.select("svg"),dl.random.uniform(0,1).samples(5));
 }
 
-function testAll(){
+var testAll = function(){
   //periodically sample through all vis types, keeping an initial random set of data.
   var data = dl.random.uniform(0,1).samples(5);
   var index = 0;
@@ -483,7 +549,7 @@ function testAll(){
   },1000);
 }
 
-function testAllGrid(){
+var testAllGrid = function(){
   //make a big line of all of our vis types, sharing an initial random set of data.
   //Click to have them go away
   var data = dl.random.uniform(0.4,1).samples(5);
@@ -516,7 +582,26 @@ var answer = function(){
   writeAnswer(participantData[questionIndex]);
 }
 
-var writeAnswer = function(response) {
+
+function genData(stimulis){
+  // TODO: Fix this to make it correctly spit out values when we've got truncation.
+  var min = stimulis.truncationLevel;
+  var n = stimulis.dataSize;
+  var sign = stimulis.trendDirection;
+  var slope = stimulis.trendSlope;
+  var max = (slope*n) + min;
+
+  var delta = (max - min)/n;
+  var vals = sign==1 ? dl.range(min,max,delta) : dl.range(max,min,-1*delta);
+  var jitter = dl.random.uniform(-delta/4,delta/4);
+
+  var constrainJitter = function(d){
+    return Math.max(Math.min(d + jitter(),1),min + delta/4);
+  }
+  return vals.map(d => constrainJitter(d));
+}
+
+var writeAnswer = function(response){
   //Called when we answer a question in the first task
   //XML to call out to a php script to store our data in a csv over in ./data/
   var writeRequest = new XMLHttpRequest();
@@ -527,7 +612,7 @@ var writeAnswer = function(response) {
   writeRequest.send();
 }
 
-function gup(name) {
+function gup(name){
   //grab our html get variables
   var regexS = "[\\?&]" + name + "=([^&#]*)";
   var regex = new RegExp( regexS );
