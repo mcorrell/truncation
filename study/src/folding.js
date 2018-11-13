be0389128800c17ecbcdbbea4f71f3b86786bfb1
mcorrell <michael.a.correll@gmail.com>
2018-11-13 15:34:13 -0800

    A little linting.

--- study/src/folding.js
+++ study/src/folding.js
@@ -4,55 +4,55 @@ var y = x;
 function makeSample(){
   var sample = d3.select("#sample");
   var icon = sample.append("svg")
-  .classed("icon",true);
+    .classed("icon",true);
 
   icon.append("rect")
-  .attr("x",1)
-  .attr("y",1)
-  .attr("width",58)
-  .attr("height",28)
-  .classed("fold",true);
+    .attr("x",1)
+    .attr("y",1)
+    .attr("width",58)
+    .attr("height",28)
+    .classed("fold",true);
 
   icon.append("rect")
-  .attr("x",1)
-  .attr("y",30)
-  .attr("width",58)
-  .attr("height",28)
-  .classed("foldDash",true);
+    .attr("x",1)
+    .attr("y",30)
+    .attr("width",58)
+    .attr("height",28)
+    .classed("foldDash",true);
 
   icon = sample.append("svg")
-  .classed("icon",true);
+    .classed("icon",true);
 
   icon.append("rect")
-  .attr("x",1)
-  .attr("y",1)
-  .attr("width",58)
-  .attr("height",28)
-  .classed("fold",true);
+    .attr("x",1)
+    .attr("y",1)
+    .attr("width",58)
+    .attr("height",28)
+    .classed("fold",true);
 
   icon.append("rect")
-  .attr("x",1)
-  .attr("y",30)
-  .attr("width",58)
-  .attr("height",28)
-  .classed("foldDash",true);
+    .attr("x",1)
+    .attr("y",30)
+    .attr("width",58)
+    .attr("height",28)
+    .classed("foldDash",true);
 
   icon.append("circle")
-  .attr("cx", 15)
-  .attr("cy",15)
-  .attr("r",4)
-  .classed("punch",true);
+    .attr("cx", 15)
+    .attr("cy",15)
+    .attr("r",4)
+    .classed("punch",true);
 
   icon = sample.append("svg")
-  .classed("icon",true);
+    .classed("icon",true);
 
   icon.append("line")
-  .attr("x1",0)
-  .attr("x2",0)
-  .attr("y1",0)
-  .attr("y2",60)
-  .attr("stroke-width","4px")
-  .attr("stroke","#333");
+    .attr("x1",0)
+    .attr("x2",0)
+    .attr("y1",0)
+    .attr("y2",60)
+    .attr("stroke-width","4px")
+    .attr("stroke","#333");
 
   var answers = [
     [{"x":20,"y":10},{"x":10,"y":20}],
@@ -98,14 +98,14 @@ function makeQuestions(){
       [{"x":10,"y":10},{"x":50,"y":10},{"x":10,"y":50},{"x":50,"y":50}],
       [{"x":20,"y":10},{"x":40,"y":10},{"x":20,"y":50},{"x":40,"y":50}],
       [{"x":10,"y":20},{"x":20,"y":10},{"x":40,"y":50},{"x":50,"y":40}],
-      [{"x":10,"y":20},{"x":20,"y":10},{"x":40,"y":50},{"x":50,"y":40},{"x":10,"y":35},{"x":25,"y":50}]
+      [{"x":10,"y":20},{"x":20,"y":10},{"x":40,"y":50},{"x":50,"y":40},{"x":10,"y":37.5},{"x":22.5,"y":50}]
     ],
     [
       //q5 answers
-      [{"x":40,"y":25},{"x":50,"y":10}],
-      [{"x":40,"y":25},{"x":50,"y":10},{"x":40,"y":35},{"x":50,"y":50}],
-      [{"x":40,"y":25},{"x":40,"y":35},{"x":50,"y":50}],
-      [{"x":40,"y":25},{"x":40,"y":35}],
+      [{"x":35,"y":25},{"x":50,"y":10}],
+      [{"x":35,"y":25},{"x":50,"y":10},{"x":35,"y":35},{"x":50,"y":50}],
+      [{"x":35,"y":25},{"x":35,"y":35},{"x":50,"y":50}],
+      [{"x":35,"y":25},{"x":35,"y":35}],
       [{"x":35,"y":35},{"x":50,"y":50}]
     ],
     [
@@ -183,7 +183,8 @@ function makeQuestions(){
   ];
 
   answers.forEach(function(q,i){
-    var sample = d3.select("body").append("div").classed("question",true);
+    var sample = d3.select("body").append("div")
+      .classed("question",true);
 
     sample.append("div")
       .style("padding-top","25px")
@@ -191,15 +192,16 @@ function makeQuestions(){
       .text("Q"+(i+1));
 
     var icon = sample.append("svg")
-    .classed("icon",true);
+      .classed("icon",true);
 
     icon.append("line")
-    .attr("x1",0)
-    .attr("x2",0)
-    .attr("y1",0)
-    .attr("y2",60)
-    .attr("stroke-width","4px")
-    .attr("stroke","#333");
+      .attr("x1",0)
+      .attr("x2",0)
+      .attr("y1",0)
+      .attr("y2",60)
+      .attr("stroke-width","4px")
+      .attr("stroke","#333");
+
     q.forEach(function(d){
       drawAnswer(sample,d);
     });
@@ -208,15 +210,15 @@ function makeQuestions(){
 
 function drawAnswer(parent,dots){
   var icon = parent.append("svg")
-  .classed("icon",true)
-  .classed("answer",true);
+    .classed("icon",true)
+    .classed("answer",true);
 
   icon.append("rect")
-  .attr("x",1)
-  .attr("y",1)
-  .attr("width",58)
-  .attr("height",58)
-  .classed("fold",true);
+    .attr("x",1)
+    .attr("y",1)
+    .attr("width",58)
+    .attr("height",58)
+    .classed("fold",true);
 
   icon.selectAll("circle").data(dots).enter().append("circle")
     .attr("cx",d=>d.x)
