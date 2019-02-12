328ac60e748f5b8b4c2a06a09d701e3aac36f90b
Michael Correll <michael.a.correll@gmail.com>
2019-02-12 11:59:36 -0800

    Setting up for Prolific

--- study/src/config.js
+++ study/src/config.js
@@ -1,4 +1,4 @@
 //If you're using prolific, you'll need to update these. Prolific codes aren't "leaky" so I'm fine committing them here.
 
-var prolific_code = "TODO";
-var prolific_URL = "https://www.prolific.ac/submissions/complete?cc=TODO";
+var prolific_code = "XEAFKCRH";
+var prolific_URL = "https://app.prolific.ac/submissions/complete?cc=XEAFKCRH";
